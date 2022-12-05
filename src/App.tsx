49074baccefa11830d1a905198a0e822287d4c00
Jenith Mercer <jamercer2012@gmail.com>
2022-12-05 15:20:37 -0600

    message displays with contrasting color

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,23 @@ function App() {
   }, []);
 
   const createNewColor = () => {
-    return '#' + Math.floor(Math.random() * 16777215).toString(16); // 256 ^ 3 base 16
+    let value = '#' + Math.floor(Math.random() * 16777215).toString(16); // 256 ^ 3 base 16
+    while (value.length < 7) value = value + '0'; // it doesn't add a 0 sometimes??
+    return value;
   };
+
+  const contrastingColor = (hexColor: string) => {
+    const pattern:RegExp = /[0-9a-f]{2}/gi;
+    const result = hexColor.match(pattern)?.map(v=>parseInt(v.toLowerCase(),16));
+    console.log(result);
+    if (result === undefined) return '#ffff00';
+    const maxValue = Math.max(...result);
+    const minValue = Math.min(...result);
+    const lum = (maxValue + minValue) / 2;
+    console.log(maxValue, minValue, lum);
+    if (lum > 127) return '#000000';
+    return '#ffffff';
+  }
 
   const createNewQuiz = () => {
     // create 3 new colors
@@ -47,7 +62,9 @@ function App() {
       </div>
       <div className="message">
         {message.text}
-        <div style={{backgroundColor: message.hex}}>{message.hex}</div>
+        <div style={{backgroundColor: message.hex, color: contrastingColor(message.hex)}}>
+          {message.hex}
+          </div>
       </div>
     </div>
   );
